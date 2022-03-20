d91fea121be6c3c41e0ce2f5f412287ef65b41b1
Sophie Clark <sophie.clark28@live.co.uk>
2022-03-20 22:10:29 +0000

    finish credit report view

--- ClearScoreTechTest/Scenes/CreditReport/CreditReportDetailsViewController.swift
+++ ClearScoreTechTest/Scenes/CreditReport/CreditReportDetailsViewController.swift
@@ -11,7 +11,9 @@ class CreditReportDetailsViewController: NiblessViewController {
     private let viewModel: CreditReportViewModel
     var scoreLabel: UILabel!
     var outOfLabel: UILabel!
+    var changesView: UIView!
     var changesContainer: UIStackView!
+    var insightsView: UIView!
     var insightsContainer: UIStackView!
     var scrollView: UIScrollView!
     
@@ -20,24 +22,154 @@ class CreditReportDetailsViewController: NiblessViewController {
         self.viewModel = viewModel
         super.init()
         setupUI()
+        setupConstraints()
+        setupChangesContainer()
+        setupInsightsContainer()
     }
     
     private func setupUI() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Credit report"
+        
+        scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        
         scoreLabel = UILabel()
         scoreLabel.text = "\(viewModel.creditScore)"
-        view.addSubview(scoreLabel)
+        scoreLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        scrollView.addSubview(scoreLabel)
         
+        outOfLabel = UILabel()
+        outOfLabel.text = viewModel.outOfText
+        scrollView.addSubview(outOfLabel)
+        
+        changesView = UIView()
+        changesView.backgroundColor = .systemGroupedBackground
+        changesView.layer.cornerRadius = 6
+        scrollView.addSubview(changesView)
+        
+        changesContainer = UIStackView()
+        changesContainer.alignment = .fill
+        changesContainer.axis = .vertical
+        changesContainer.spacing = 4
+        changesView.addSubview(changesContainer)
+        
+        insightsView = UIView()
+        insightsView.layer.borderWidth = 1
+        insightsView.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        insightsView.layer.cornerRadius = 6
+        scrollView.addSubview(insightsView)
+        
+        insightsContainer = UIStackView()
+        insightsContainer.alignment = .fill
+        insightsContainer.axis = .vertical
+        insightsContainer.spacing = 4
+        insightsView.addSubview(insightsContainer)
     }
     
     private func setupConstraints() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.topAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        scoreLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        
+        outOfLabel.translatesAutoresizingMaskIntoConstraints = false
+        outOfLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 4).isActive = true
+        outOfLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        
+        changesView.translatesAutoresizingMaskIntoConstraints = false
+        changesView.topAnchor.constraint(equalTo: outOfLabel.bottomAnchor, constant: 8).isActive = true
+        changesView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 8).isActive = true
+        changesView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -8).isActive = true
+        changesView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -16).isActive = true
+        
+        changesContainer.translatesAutoresizingMaskIntoConstraints = false
+        changesContainer.topAnchor.constraint(equalTo: changesView.topAnchor, constant: 8).isActive = true
+        changesContainer.leadingAnchor.constraint(equalTo: changesView.leadingAnchor, constant: 8).isActive = true
+        changesContainer.trailingAnchor.constraint(equalTo: changesView.trailingAnchor, constant: -8).isActive = true
+        changesContainer.widthAnchor.constraint(equalTo: changesView.widthAnchor, constant: -16).isActive = true
+        changesContainer.bottomAnchor.constraint(equalTo: changesView.bottomAnchor, constant: -8).isActive = true
+        
+        insightsView.translatesAutoresizingMaskIntoConstraints = false
+        insightsView.topAnchor.constraint(equalTo: changesView.bottomAnchor, constant: 8).isActive = true
+        insightsView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 8).isActive = true
+        insightsView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -8).isActive = true
+        insightsView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -16).isActive = true
+        
+        insightsContainer.translatesAutoresizingMaskIntoConstraints = false
+        insightsContainer.topAnchor.constraint(equalTo: insightsView.topAnchor, constant: 8).isActive = true
+        insightsContainer.leadingAnchor.constraint(equalTo: insightsView.leadingAnchor, constant: 8).isActive = true
+        insightsContainer.trailingAnchor.constraint(equalTo: insightsView.trailingAnchor, constant: -8).isActive = true
+        insightsContainer.bottomAnchor.constraint(equalTo: insightsView.bottomAnchor, constant: -8).isActive = true
     }
     
     private func setupChangesContainer() {
+        changesContainer.addArrangedSubview(makeSeparatorLine())
+        let titleLabel = UILabel()
+        titleLabel.text = viewModel.changesTitle
+        titleLabel.font = .preferredFont(forTextStyle: .headline)
+        changesContainer.addArrangedSubview(titleLabel)
         
+        if viewModel.hasShortTermDebt || viewModel.shortTermDebtChange != 0 {
+            let shortTermTitleLabel = UILabel()
+            shortTermTitleLabel.numberOfLines = 0
+            shortTermTitleLabel.text = "\(viewModel.shortTermDebtTitle) £\(viewModel.shortTermDebtValue)"
+            
+            changesContainer.addArrangedSubview(shortTermTitleLabel)
+    
+            let shortTermChangedByLabel = UILabel()
+            shortTermChangedByLabel.numberOfLines = 0
+            if viewModel.shortTermDebtChange != 0 {
+                shortTermChangedByLabel.text = "\(viewModel.shortTermDebtChangeTitle) £\(viewModel.shortTermDebtChange)"
+            } else {
+                shortTermChangedByLabel.text = viewModel.shortTermDebtChangeTitle
+            }
+            changesContainer.addArrangedSubview(shortTermChangedByLabel)
+        }
+        if viewModel.hasLongTermDebt || viewModel.longTermDebtChange != 0 {
+            let longTermTitleLabel = UILabel()
+            longTermTitleLabel.numberOfLines = 0
+            longTermTitleLabel.text = "\(viewModel.longTermDebtTitle) £\(viewModel.longTermDebtValue)"
+            
+            changesContainer.addArrangedSubview(longTermTitleLabel)
+            
+            let longTermChangedByLabel = UILabel()
+            longTermChangedByLabel.numberOfLines = 0
+            if viewModel.longTermDebtChange != 0 {
+                longTermChangedByLabel.text = "\(viewModel.longTermDebtChangeTitle) £\(viewModel.longTermDebtChange)"
+            } else {
+                longTermChangedByLabel.text = "\(viewModel.longTermDebtChangeTitle)"
+            }
+            changesContainer.addArrangedSubview(longTermChangedByLabel)
+        }
+        changesContainer.addArrangedSubview(makeSeparatorLine())
     }
     
     private func setupInsightsContainer() {
+        let titleLabel = UILabel()
+        titleLabel.text = viewModel.insightsTitle
+        titleLabel.font = .preferredFont(forTextStyle: .headline)
+        insightsContainer.addArrangedSubview(titleLabel)
         
+        let delinquentLabel = UILabel()
+        delinquentLabel.text = viewModel.missedPayemntsText
+        insightsContainer.addArrangedSubview(delinquentLabel)
+        
+        let defaultsLabel = UILabel()
+        defaultsLabel.text = viewModel.defaultedText
+        insightsContainer.addArrangedSubview(defaultsLabel)
+        
+    }
+    
+    private func makeSeparatorLine() -> UIView {
+        let separatorLine = UIView()
+        separatorLine.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        separatorLine.layer.borderWidth = 2
+        return separatorLine
     }
 }
--- ClearScoreTechTest/Scenes/CreditReport/CreditReportViewModel.swift
+++ ClearScoreTechTest/Scenes/CreditReport/CreditReportViewModel.swift
@@ -12,18 +12,28 @@ struct CreditReportViewModel {
     private let creditAccount: CreditAccount
     
     //header
-    var creditScore: Int
-    var maxScore: Int
+    let creditScore: Int
+    let outOfText: String
     
     //changes
-    var hasShortTermDebt: Bool
-    var shortTermDebtChange: Int
-    var hasLongTermDebt: Bool
-    var longTermDebtChange: Int
+    let changesTitle: String
+    let hasShortTermDebt: Bool
+    let shortTermDebtTitle: String
+    let shortTermDebtChangeTitle: String
+    let shortTermDebtChange: Int
+    let shortTermDebtValue: Int
+    let longTermDebtTitle: String
+    let longTermDebtChangeTitle: String
+    let hasLongTermDebt: Bool
+    let longTermDebtChange: Int
+    let longTermDebtValue: Int
     
     //insights
-    var monthsSinceLastMissedPayment: Int?
-    var hasEverDefaulted: Bool
+    let insightsTitle: String
+    let monthsSinceLastMissedPayment: Int
+    let missedPayemntsText: String
+    let hasEverDefaulted: Bool
+    let defaultedText: String
     
     //credit utilisation
     
@@ -32,13 +42,54 @@ struct CreditReportViewModel {
         self.creditAccount = creditAccount
         
         self.creditScore = creditAccount.creditReportInfo.score
-        self.maxScore = creditAccount.creditReportInfo.maxScoreValue
+        self.outOfText =  String(format: "scoreview.bottom.label".localized(), creditAccount.creditReportInfo.maxScoreValue) 
         
+        self.changesTitle = String(localized: "creditreport.changes.title")
+        
+        self.shortTermDebtTitle = String(localized: "creditreport.shorttermdebt.title")
         self.hasShortTermDebt = creditAccount.creditReportInfo.currentShortTermDebt <= 0
         self.shortTermDebtChange = creditAccount.creditReportInfo.changeInShortTermDebt
+        self.shortTermDebtValue = creditAccount.creditReportInfo.currentShortTermDebt
+        if shortTermDebtChange > 0 {
+            self.shortTermDebtChangeTitle = String(localized: "creditreport.shorttermdebt.increased.text")
+        } else if shortTermDebtChange < 0 {
+            self.shortTermDebtChangeTitle = String(localized: "creditreport.shorttermdebt.decreased.text")
+        } else {
+            self.shortTermDebtChangeTitle = String(localized: "creditreport.shorttermdebt.nochange.text")
+        }
+        
         self.hasLongTermDebt = creditAccount.creditReportInfo.currentLongTermDebt <= 0
         self.longTermDebtChange = creditAccount.creditReportInfo.changeInLongTermDebt
+        self.longTermDebtTitle = String(localized: "creditreport.longtermdebt.title")
+        if longTermDebtChange > 0 {
+            self.longTermDebtChangeTitle = String(localized: "creditreport.longtermdebt.increased.text")
+        } else if longTermDebtChange < 0 {
+            self.longTermDebtChangeTitle = String(localized: "creditreport.longtermdebt.decreased.text")
+        } else {
+            self.longTermDebtChangeTitle = String(localized: "creditreport.longtermdebt.nochange.text")
+        }
         
+        self.longTermDebtValue = creditAccount.creditReportInfo.currentLongTermDebt
+        
+        self.insightsTitle = String(localized: "creditreport.insights.title")
         self.hasEverDefaulted = creditAccount.creditReportInfo.hasEverDefaulted
+        if hasEverDefaulted {
+            if creditAccount.creditReportInfo.monthsSinceLastDefaulted > 1 {
+                self.defaultedText = String(format: "creditreport.insights.defaults.pluralmonths.text".localized(), creditAccount.creditReportInfo.monthsSinceLastDefaulted)
+            } else {
+                self.defaultedText = String(format: "creditreport.insights.defaults.onemonth.text".localized())
+            }
+        } else {
+            self.defaultedText = String(localized: "creditreport.insights.nodefaults.text")
+        }
+        
+        self.monthsSinceLastMissedPayment = creditAccount.creditReportInfo.monthsSinceLastDelinquent
+        if creditAccount.creditReportInfo.hasEverBeenDelinquent {
+            missedPayemntsText = String(localized: "creditreport.insights.nomissedpayments.text")
+        } else if monthsSinceLastMissedPayment <=  1 {
+            missedPayemntsText = String(format: "creditreport.insights.missedpayments.pluralmonths.text", monthsSinceLastMissedPayment)
+        } else {
+            missedPayemntsText = String(localized: "creditreport.insights.missedpayments.onemonth.text")
+        }
     }
 }
